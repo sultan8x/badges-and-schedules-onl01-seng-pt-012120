9333c4c0eec49a5142249db806a191e6c21753f5
Sultan Afsheen <leadsultan8x@gmail.com>
2020-01-31 09:00:28 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,20 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+
+attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus","Matz"]
+
+def batch_badge_creator(attendees)
+    attendees.collect do |person|
+      p "Hello, my name is #{person}."
+    end
+end
+
+def assign_rooms(attendees)
+  count = 1
+  attendees.collect do |person|
+    p "Hello, #{person}! You'be be assigned to room #{count}!"
+    count += 1
+  end
+end
