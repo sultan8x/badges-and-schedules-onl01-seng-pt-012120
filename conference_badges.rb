becacc2ff09f8f0a4a7774ad5fca027c89c6e9b7
Sultan Afsheen <leadsultan8x@gmail.com>
2020-02-01 01:54:41 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -16,6 +16,7 @@ end
 
 
 def assign_rooms(attendees)
+<<<<<<< HEAD
  assigned_rooms = []
   attendees.each.with_index(1) do |person, index|
      assigned_rooms << "Hello, #{person}! You'll be assigned to room #{index}!"
@@ -32,5 +33,11 @@ def printer(attendees)
   end
   assign_rooms(attendees).collect do |room|
     puts room
+=======
+  attendees.each.with_index(1) do |person, index|
+    p "Hello, #{person}! You'll be assigned to room #{index}!"
+>>>>>>> 4bad843eb09b6a4b235e48b868020d050186f268
   end
+
 end
+
